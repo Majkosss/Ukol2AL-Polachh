7259bb1343872c6a975a5890edfaa00778bee8cd
Michal P <159123359+Majkosss@users.noreply.github.com>
2024-11-20 10:28:33 +0000

    Final-Ukol

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-import random
+array = [10, 20, 48, 52, 75, 60, 12, 8, 6, 1]
+print("Základní pole:", array)
 
-pole = [10, 20, 48, 52, 75, 60, 12, 8, 6, 1]
-print("Základní pole:", pole)
+def bubble_sort():
+    n = len(array)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+            print(array)
+    return array
 
-sort_pole = sorted(pole)
-print("Seřazené pole:", sort_pole)
+print(bubble_sort())
